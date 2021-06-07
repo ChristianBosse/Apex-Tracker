8da340683a18a55c55de11e3d53bbe6d947dbb73
Christian Bosse <christianbosse1998@gmail.com>
2021-06-07 16:19:52 -0300

    Header and Nav 75% Done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,12 @@ import React from "react";
 import styled from "styled-components";
 const Nav = () => {
   return (
-    <>
-      <Wrapper></Wrapper>
-    </>
+    <Wrapper>
+      <Home>HOME</Home>
+      <PlayerSearch>PLAYER LOOKUP</PlayerSearch>
+      <Randomizer>RANDOMIZER</Randomizer>
+      <About>ABOUT</About>
+    </Wrapper>
   );
 };
 
@@ -12,6 +15,42 @@ const Wrapper = styled.div`
   height: 45px;
   width: 100vw;
   background-color: black;
+  color: white;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const Home = styled.a`
+  font-size: 22px;
+  margin: 0px 20px;
+  &:hover {
+    color: #da292a;
+  }
+`;
+
+const PlayerSearch = styled.a`
+  font-size: 22px;
+  margin: 0px 20px;
+  &:hover {
+    color: #da292a;
+  }
+`;
+
+const Randomizer = styled.a`
+  font-size: 22px;
+  margin: 0px 20px;
+  &:hover {
+    color: #da292a;
+  }
+`;
+
+const About = styled.a`
+  font-size: 22px;
+  margin: 0px 20px;
+  &:hover {
+    color: #da292a;
+  }
 `;
 
 export default Nav;
